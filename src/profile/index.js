4a2f55568e387d6048c1db7dde0e49823aa2d71d
Sabongi <93168272+Sabongi@users.noreply.github.com>
2022-09-20 20:51:09 -0300

    Profile quase feito

--- src/profile/index.js
+++ src/profile/index.js
@@ -5,7 +5,19 @@ export default function Perfil() {
     return(
         <View style={styles.container}>
             <View style={styles.caixa}> 
-            <Text>a</Text>
+                <View style={styles.header}>
+                    <View style={styles.foto}>   
+                    </View>
+                    <View style={styles.info}>
+                        <Text style={styles.nome}>Olá Cleiton</Text>
+                        <Text style={styles.local}>Pindamonhangaba</Text>
+                    </View>
+                </View>
+
+                {/* <View style={styles.body}>
+
+                </View> */}     
+                
             </View>
         </View>
     );
--- src/profile/styles.js
+++ src/profile/styles.js
@@ -17,6 +17,49 @@ const styles = StyleSheet.create({
         margin: 13,
         borderRadius: 20,
         width: '96%',
+        shadowRadius:12,
+        shadowColor:'black'
+    },
+    header:{
+        
+        backgroundColor:'#000',
+        width:'100%',
+        height:'17%',
+        bottom: 258,
+        flexDirection:'row'
+    },
+    foto:{
+        backgroundColor:'red',
+        height:'100%',
+        width: '27%',
+        borderRadius:100,
+    },
+    info:{
+        backgroundColor:'green',
+        height:'100%',
+        width:'73%'
+    },
+    nome:{
+        fontSize:22,
+        fontWeight:'bold',
+        alignContent:'center',
+        justifyContent:'center',
+        marginTop:15,
+        marginLeft:7    
+    },
+    local:{
+        fontSize:15,
+        alignContent:'center',
+        justifyContent:'center',
+        marginTop:15,
+        marginLeft:7  
+    },
+    body:{
+        top: 100,
+        position:'relative',
+        backgroundColor:'blue',
+        width:'100%',
+        height:'70%',
     },
     text: {
         fontSize: 16,
