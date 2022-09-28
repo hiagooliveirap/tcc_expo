0f2a06cc2b1fafb07782a67ea2489dc88139ab92
Sabongi <93168272+Sabongi@users.noreply.github.com>
2022-09-28 19:58:44 -0300

    Mudei a cor do favoritos

--- src/favoritos/styles.js
+++ src/favoritos/styles.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
         flex: 1,
         //justifyContent: 'center',
         //alignItems: 'center',
-        backgroundColor: '#F7DED9',
+        backgroundColor: '#DEDADA',
         margin: 13,
         borderRadius: 20,
         width: '96%',
