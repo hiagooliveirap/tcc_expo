b4015da9578551664529728b3cd54b630b3d260a
Bruno Leite <85137185+brunoleitee@users.noreply.github.com>
2022-10-03 20:06:14 -0300

    Fonte Poppins

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/login';
+import { useFonts, Poppins_400Regular, Poppins_600SemiBold, Poppins_500Medium } from '@expo-google-fonts/poppins';
 import Constants from 'expo-constants';
 
 import Rotas from './src/rotas/rotas';
 // import Teste from './src/cadCliente';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Poppins_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-      <Rotas />  
+    <Rotas />
   );
 }
 
