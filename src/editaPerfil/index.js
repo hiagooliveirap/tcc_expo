713e40f87dd92712775169421571ba0070a307f0
Bruno Leite <85137185+brunoleitee@users.noreply.github.com>
2022-10-31 21:55:31 -0300

    Editar Perfil

--- src/editaPerfil/index.js
+++ src/editaPerfil/index.js
@@ -1,20 +1,22 @@
 import { Text, View, TouchableOpacity, Image, TextInput, KeyboardAvoidingView } from 'react-native';
 import styles from './styles';
 
-export default function EditaPerfil({navigation}) {
-    return(
+export default function EditaPerfil({ navigation }) {
+    return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Perfil')}>
-                    <Image source={require('../../assets/arrow-back-black.png')} />
-                </TouchableOpacity>            
+                    <TouchableOpacity onPress={() => navigation.navigate('Perfil')}>
+                        <Image source={require('../../assets/arrow-back-black.png')} />
+                    </TouchableOpacity>
                 </TouchableOpacity>
 
-                <Text style={styles.text_header}>Editar perfil</Text>                     
-           </View>              
+                <Text style={styles.text_header}>Editar perfil</Text>
+            </View>
 
-           <KeyboardAvoidingView style={styles.container_input}>
+            <Image source={require('../../assets/profile.jpg')} style={styles.foto} />
+
+            <KeyboardAvoidingView style={styles.container_input}>
                 <TextInput
                     style={styles.inputBusca}
                     placeholder='Nome'
@@ -27,12 +29,13 @@ export default function EditaPerfil({navigation}) {
                     placeholder='Email'
                     placeholderTextColor='#C5C5C6'
                     underlineColorAndroid='transparent'
-                />                
-           </KeyboardAvoidingView>
+                />
+            </KeyboardAvoidingView>
 
-           <TouchableOpacity style={styles.btn_salvar}>
-                    <Text style={styles.text_btn_salvar}>Salvar</Text>
+            <TouchableOpacity style={styles.btn_salvar}>
+                <Text style={styles.text_btn_salvar}>Salvar</Text>
             </TouchableOpacity>
+
         </View>
     )
 }
