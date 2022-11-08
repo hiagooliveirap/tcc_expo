b506e1a173df5b812f9a65a4a4df71de59cdf1aa
Sabongi <93168272+Sabongi@users.noreply.github.com>
2022-11-07 22:32:30 -0300

    Tela de perfil refeita

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,43 +1,72 @@
 import { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity, Image, FlatList, ScrollView } from 'react-native';
+import { Text, View, TouchableOpacity, Image, FlatList, ScrollView, KeyboardAvoidingView, TextInput } from 'react-native';
 import styles from './styles';
 import CardItemHorizontal from '../produtos/card_item_horizontal';
 import img1 from '../../assets/product-7.jpg';
 
-import api from '../services/api.js';
+import api from '../services/api';
 
 export default function Perfil({ navigation, route, at }) {
+    const [exibe, setExibe] = useState(0);
+    // const [nome, setNome] = useState(route.params.name);
+    // const [email,setEmail] = useState(route.params.email);
 
+    // const mudaTela = (tela) =>{
+    //     setExibe(tela)
+    // }
+
+    //  async function atCadastro() {  
+    //     let alterou = false;
+    //     try {
+          
+    //       const response = await api.get('usuarios/' + id);
+    //       alterou = response.data.confirma;
+    //       setNome(response.data.message.usuNome);
+    //       setEmail(response.data.message.usuEmail);
+    //       console.log(email); 
+    //     } catch (err) {
+    //         console.log('Erro: ' + err);
+    //         return false;
+    //     } 
+        
+    //         if (!alterou) {
+    //             alert('Falha na atualização');
+    //         }
+        
+    //   }
+
+
+    //     useEffect(() => {
+    //     atCadastro();
+    // }, [exibe]);
+
+    
+    return(
+        <View style={styles.container}>
+            {
+                exibe === 0
+                ?
+                    <VerPerfil mudaTela={mudaTela} route={route} navigation={navigation} />    
+                :
+                    <MudarPerfil mudaTela={mudaTela} route={route} navigation={navigation} />
+            }
+
+            
+        </View>
+    )
+
+
+}
+
+
+function VerPerfil({mudaTela, route, navigation}){
     const id = route.params.id;
     const [nome, setNome] = useState(route.params.name);
     const [email,setEmail] = useState(route.params.email);
     const item = {id, nome, email};
     const [tipoSel, setTipoSel] = useState([]);
 
-    async function atCadastro() {  
-        let alterou = false;
-        try {
-          
-          const response = await api.get('usuarios/' + id);
-          alterou = response.data.confirma;
-          setNome(response.data.message.usuNome);
-          setEmail(response.data.message.usuEmail);
-          console.log(email); 
-        } catch (err) {
-            console.log('Erro: ' + err);
-            return false;
-        } 
-        
-            if (!alterou) {
-                alert('Falha na atualização');
-            }
-        
-      }
-
-      useEffect(() => {
-        atCadastro();
-      }, [at]);
-
+   
     
 
     //const [tipoProduto, setTipoProduto] = useState(['Tipo', 'Lanche', 'Porção', 'Suco']); 
@@ -69,6 +98,8 @@ export default function Perfil({ navigation, route, at }) {
         ]
     );
 
+
+
     return (
         <ScrollView
             style={styles.container}
@@ -78,7 +109,7 @@ export default function Perfil({ navigation, route, at }) {
 
                     <Text style={{ fontSize: 17, fontWeight: 'bold', }}>Perfil</Text>
 
-                    <TouchableOpacity onPress={() => navigation.navigate('EditPerfil', {item, atCadastro:atCadastro})}>
+                    <TouchableOpacity onPress={() => mudaTela(1)}>
                         <Image source={require('../../assets/edit-button-black.png')} />
                     </TouchableOpacity>
                 </View>
@@ -113,4 +144,72 @@ export default function Perfil({ navigation, route, at }) {
             </View>
         </ScrollView>
     );
+}
+
+function MudarPerfil({route, mudaTela}){
+    const id = route.params.usuId;
+    const [nome, setNome] = useState(route.params.nome);    
+    const [email, setEmail] = useState(route.params.email);   
+
+    async function attCadastro() {  
+        let alterou = false;
+        try {
+          let dadosUsu = {
+            nome, 
+            email
+          };
+          const response = await api.patch('usuarios/' + id, dadosUsu);
+          alterou = response.data.confirma; 
+          console.log('AAAAAAAAAAAAAAAAAAAAAAAAA' + alterou);
+        } catch (err) {
+            console.log('Erro: ' + err);
+        }
+    
+        if (alterou) {
+            // route.params.email = email;
+            mudaTela(0);
+        } else {
+          alert('Falha no cadastro');
+        }
+        
+      }
+
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity>
+                    <TouchableOpacity onPress={() => mudaTela(0)}>
+                        <Image source={require('../../assets/arrow-back-black.png')} />
+                    </TouchableOpacity>
+                </TouchableOpacity>
+
+                <Text style={styles.text_header}>Editar perfil</Text>
+            </View>
+
+            <KeyboardAvoidingView style={styles.container_input}>
+                <TextInput
+                    style={styles.inputBusca}
+                    value={nome}
+                    onChangeText={(n) => setNome(n) }
+                    placeholderTextColor='#C5C5C6'
+                    underlineColorAndroid='transparent'
+                />
+
+                <TextInput
+                    style={styles.inputBusca}
+                    value={email}
+                    onChangeText={(e) => setEmail(e) }
+                    placeholderTextColor='#C5C5C6'
+                    underlineColorAndroid='transparent'
+                />
+            </KeyboardAvoidingView>
+
+            <TouchableOpacity style={styles.btn_salvar} onPress={() => attCadastro()}>
+                <Text style={styles.text_btn_salvar}>Salvar</Text>
+            </TouchableOpacity>
+
+        </View>
+    )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://10.67.23.73:3333'
+    baseURL: 'http://10.67.23.228:3333'
 });
 
 export default api;
